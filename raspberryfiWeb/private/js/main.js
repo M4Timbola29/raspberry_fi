25d30f97ecbed9a604422ba9f34e086ffe5195e9
Rúben Príncipe <36694769+M4Timbola29@users.noreply.github.com>
2022-12-29 03:15:02 +0000

    jquery complete ready to merge into main

--- raspberryfiWeb/private/js/main.js
+++ raspberryfiWeb/private/js/main.js
@@ -1,4 +1,40 @@
 try {
+	$("#link-dashboard").on("click", (e) => {
+		e.preventDefault();
+
+		$("#dynamicName").text("Dashboard");
+		$(".content").load("/html/dashboard/index.html");
+
+		window.history.pushState("string", "dashboard", "/dashboard");
+	});
+
+	$("#link-wireless").on("click", (e) => {
+		e.preventDefault();
+
+		$("#dynamicName").text("Wireless");
+		$(".content").load("/html/wireless/index.html");
+
+		window.history.pushState("string", "wireless", "/wireless");
+	});
+
+	$("#link-dhcp").on("click", (e) => {
+		e.preventDefault();
+
+		$("#dynamicName").text("DHCP");
+		$(".content").load("/html/dhcp/index.html");
+
+		window.history.pushState("string", "dhcp", "/dhcp");
+	});
+
+	$("#link-openVPN").on("click", (e) => {
+		e.preventDefault();
+
+		$("#dynamicName").text("OpenVPN");
+		$(".content").load("/html/openVPN/index.html");
+
+		window.history.pushState("string", "openVPN", "/openVPN");
+	});
+
 	$("#link-gameServers").on("click", (e) => {
 		e.preventDefault();
 
@@ -19,6 +55,18 @@ try {
 
 	$(() => {
 		switch (window.location.pathname) {
+			case "/dashboard":
+				$(".content").load("/html/dashboard/index.html");
+				break;
+			case "/wireless":
+				$(".content").load("/html/wireless/index.html");
+				break;
+			case "/dhcp":
+				$(".content").load("/html/dhcp/index.html");
+				break;
+			case "/openVPN":
+				$(".content").load("/html/openVPN/index.html");
+				break;
 			case "/gameServers":
 				$(".content").load("/html/gameServers/index.html");
 				break;
