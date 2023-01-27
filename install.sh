c60fdaefc0e02a6ecb2476fbfaafc185b6b20ed1
Rúben Príncipe <36694769+M4Timbola29@users.noreply.github.com>
2023-01-27 16:05:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,14 @@
 #move raspberryfiWeb to /var/www/html/
 #install nvm, nodejs, npm, yarn as global, build and host the project
 #create .env file in /var/www/html/raspberryfiWeb
+#install docker and create the servers on containers
+#create .env variables
+
+echo "  _____                 _                          ______ _  "
+echo " |  __ \               | |                        |  ____(_) "
+echo " | |__) |__ _ ___ _ __ | |__   ___ _ __ _ __ _   _| |__   _  "
+echo " |  _  // _\` / __| '_ \| '_ \ / _ \ '__| '__| | | |  __| | | "
+echo " | | \ \ (_| \__ \ |_) | |_) |  __/ |  | |  | |_| | |    | | "
+echo " |_|  \_\__,_|___/ .__/|_.__/ \___|_|  |_|   \__, |_|    |_| "
+echo "                 | |                          __/ |          "
+echo "                 |_|                         |___/           "
